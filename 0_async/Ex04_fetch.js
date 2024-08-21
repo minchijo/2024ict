861974b74aab26878d70c391359ccb69b3d3d3e9
minchi <87708449+minchijo@users.noreply.github.com>
2024-08-21 17:37:07 +0900

    Delete 0_async directory

--- 0_async/Ex04_fetch.js
+++
@@ -1,7 +0,0 @@
-//Ex04_fetch.js
-
-fetch("http://jsonplaceholder.typicode.com/users") // 응답받은 body데이터의 form을 직접 변환해야 하
-.then(response => response.json() ) //response는 선생님이 정한 변수명, 문자열을 제이슨으로 변환
-.then( users => console.log(users))
-
-console.log("다른 작업")
